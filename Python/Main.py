b8ff11384832fab0cf83a7a54eac8dbeaf3d4541
Bukreev Aleksandr <bukreev.alex.02@gmail.com>
2023-11-02 15:55:04 +0300

    Сделал расчет Rastr и БД

--- Python/Main.py
+++ Python/Main.py
@@ -18,39 +18,41 @@ elements = [[51700109, 40303999], [52902023, 40303061],
 vetvs = [[51700026, 51700222, 0], [40502010, 40502531, 0]]
 
 # РАСЧЕТ РАСТРА
-#------------------------------------------------
+
+# ------------------------------------------------
 # Функция получения данных исходного режима
+
+
 def norm_F():
     print("Расчет исходного режима")
-    norm_P_mass=Rastr_Calc_class(path_mode, path_sech, path_trajectory).norm_mode()
-
-    # Создаём список кортежей элементов для передачи в БД
-    tuple_n_P_mass=[(x,) for x in norm_P_mass]
+    norm_P_mass = Rastr_Calc_class(
+        path_mode, path_sech, path_trajectory).norm_mode()
     # Занесение значений в БД
     bd_script.new_tab("Norm_mode")
-    bd_script.data_P("Norm_mode",tuple_n_P_mass)
+    bd_script.data_P("Norm_mode", norm_P_mass)
 
-#------------------------------------------------
+# ------------------------------------------------
 # Функция получения данных ПА режима
+
+
 def emerg_F():
     print("Расчет ПА режима с НВ")
-    k=0
+    k = 1
     for i in elements:
-        name="RI_"+str(k)
+        name = "RI_"+str(k)
         # Называем каждую ветвь
-        j=1
-        name_vetvs=[]
+        j = 1
+        name_vetvs = []
         for vl in vetvs:
             name_vetvs.append("VL"+str(j))
-            j=j+1
-        bd_script.new_tab(name,name_vetvs)
+            j = j+1
+        bd_script.new_tab(name, name_vetvs)
         P_I_one_mass = Rastr_Calc_class(
             path_mode, path_sech, path_trajectory).emerg_mode(i, vetvs)
-        # Создаём список кортежей элементов для передачи в БД
-        tuple_RI_P_mass=[(x,) for x in P_I_one_mass[0]]
+        if k == 3:
+            print(P_I_one_mass[1])
         # Занесение значений в БД
-        bd_script.data_P(name,tuple_RI_P_mass)
-        # bd_script.data_I(name,tuple_RI_P_mass)
-        k+=1
-
-emerg_F()
+        print(name)
+        bd_script.data_P(name, P_I_one_mass[0])
+        bd_script.data_I(name, P_I_one_mass[1], name_vetvs)
+        k += 1
--- Python/bd_script.py
+++ Python/bd_script.py
@@ -2,42 +2,65 @@ import sqlite3 as sl
 
 # Создание и/или подключение файла БД
 bd = sl.connect("Python//data.db")
-cur=bd.cursor()
+cur = bd.cursor()
 # Создание новой таблицы и стобцов
 
 
 def new_tab(name, vetv=False):
     with bd:
         # Создание таблицы
-        table = "CREATE TABLE if not exists "+name+" (P_sech);"
+        table = "CREATE TABLE if not exists "+name+" (id,P_sech);"
         bd.execute(table)
-        
-        cur.execute('PRAGMA table_info(Norm_mode)')     # Получаем выборку всех имеющихся столбцов
-        cols = cur.fetchall()                           # Забираем все столбцы в переменную
+
+        # Получаем выборку всех имеющихся столбцов
+        cur.execute(f'PRAGMA table_info({name})')
+        # Забираем все столбцы в переменную
+        cols = cur.fetchall()
         # Добавление столбцов по току контролируемых ветвей, если только их ещё нет
-        print(vetv)
-        if vetv!=False:
+        if vetv != False:
             for vl in vetv:
-                flag=False
+                flag = False
                 for c in cols:
-                    if vl==c[1]:
-                        flag=True
-                if flag==False:
+                    if vl == c[1]:
+                        flag = True
+                if flag == False:
                     col = 'AlTER TABLE '+name+' ADD COLUMN '+vl
                     bd.execute(col)
 
+# ------------------------------------------------
 # Функции вноса данных
 # Значения P
-def data_P(name,mass_P):
+
+
+def data_P(name, mass_P):
     with bd:
-        # Предварительная очистка таблицы
+        # Предварительная чистка таблицы
         bd.execute("DELETE FROM "+name)
-        # Вносим данные в БД
-        new_data_P="INSERT INTO "+name+" (P_sech) values(?)"
-        bd.executemany(new_data_P,mass_P)
-        
+        ind = [(x,) for x in range(1, len(mass_P)+1)]
+        # Вносим индексы значений
+        new_data_ind = "INSERT INTO "+name+" (id) values(?)"
+        bd.executemany(new_data_ind, ind)
+        # Обновляем значения P
+        for i in ind:
+            bd.execute(f"UPDATE {name} SET P_sech=? WHERE id={
+                       i[0]}", (mass_P[i[0]-1],))
+
+# ------------------------------------------------
 # Значения I
-def data_I(name,vetv,mass_I):
+# Данную функцию необходимо выполнять после функции data_P
+# Потому, что в data_P создаются строки и задаются к ним индексы,
+# А в этой функции происходит обновление (а не добавление) данных по току
+
+
+def data_I(name, mass_I, vetv):
+    with bd:
+        # Выборка всех ID
+        cur.execute(f"SELECT id FROM {name}")
+        ind = cur.fetchall()
+        k = 0
+        # Обновление данных I в ветвях
         for vl in vetv:
-            new_data_I="INSERT INTO "+name+" ("+vl+") values(?)"
-        bd.executemany(new_data_I,mass_I)
+            for i in ind:
+                bd.execute(f"UPDATE {name} SET {vl}= ? WHERE id={
+                           i[0]}", (mass_I[k][i[0]-1],))
+            k += 1
